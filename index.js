7d59eab92a4ab861f1de3afc2ccf1190ee118f01
Kirill Andreev <kenokoz@yandex.ru>
2022-01-03 16:13:52 +0300

    change the structure

--- index.js
+++ index.js
@@ -1,49 +1,11 @@
 const http = require('http');
 const EventEmitter = require('events');
+const Router = require('./framework/Router');
+const Application = require('./framework/Application');
 
 const PORT = process.env.PORT || 5000;
 
-const emitter = new EventEmitter();
-
-class Router {
-  constructor() {
-    this.endpoints = {};
-  }
-
-  request(method = 'GET', path, handler) {
-    if (!this.endpoints[path]) {
-      this.endpoints[path] = {};
-    }
-
-    // /users [GET, POST, PUT] /posts [GET, POST, PUT, DELETE]
-    const endpoint = this.endpoints[path];
-
-    if (endpoint[method]) {
-      throw new Error(`[${method}] по адресу ${path} уже существует`);
-    }
-
-    endpoint[method] = handler;
-    emitter.on(`[${path}]:[${method}]`, (req, res) => {
-      handler(req, res);
-    });
-  }
-
-  get(path, handler) {
-    this.request('GET', path, handler);
-  }
-
-  post(path, handler) {
-    this.request('POST', path, handler);
-  }
-
-  put(path, handler) {
-    this.request('PUT', path, handler);
-  }
-
-  delete(path, handler) {
-    this.request('DELETE', path, handler);
-  }
-}
+const app = new Application();
 
 const router = new Router();
 
@@ -55,10 +17,8 @@ router.get('/posts', (req, res) => {
   res.end('Hello posts');
 });
 
-const server = http.createServer((req, res) => {
-  emitter.emit(`[${req.url}]:[${req.method}]`, req, res);
-});
+app.addRouter(router);
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log('Server is started');
 });
